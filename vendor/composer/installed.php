2cd32185ba3a32d13587a19d21d3ee85804e49c7
toantd14 <toantd.work@gmail.com>
2022-05-29 16:48:38 +0700

    change-version-php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bf8f6a60024af7e517a71b3469cd39cdceb83dda',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bf8f6a60024af7e517a71b3469cd39cdceb83dda',
+            'dev_requirement' => false,
+        ),
+    ),
+);
